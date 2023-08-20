4dfd47cc286c0f445928365fbb976e405c3fbebf
Popilworck <89626573+Popilworck@users.noreply.github.com>
2023-08-20 14:48:58 +0530

    Add files via upload

---
+++ gui2.py
@@ -0,0 +1,66 @@
+import os
+from pathlib import Path
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage,Label
+OUTPUT_PATH = Path(__file__).parent
+p=os.getcwd()
+ASSETS_PATH = OUTPUT_PATH / Path(rf"{p}\assets\frame0")
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+from tkinter import filedialog
+from PIL import Image, ImageTk
+k=False
+photo =str()
+def open_image():
+    global k
+    if k == True:
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
+        if file_path:
+            print(file_path)
+            image = Image.open(file_path)
+            image.thumbnail((1280*0.7, 720)) 
+            photo = ImageTk.PhotoImage(image)
+            return photo
+    else:
+        file_path = rf"{p}/assets/g20.jpeg"
+        if file_path:
+            image = Image.open(file_path)
+            image.thumbnail((1280*0.7, 720)) 
+            photo = ImageTk.PhotoImage(image)
+            k= True
+            return photo
+       
+def get_photo():
+    photo = open_image()
+    if photo:
+        image_label.config(image=photo)
+        image_label.photo = photo  
+    else:
+        image_label.config(image=None)
+
+window = Tk()
+
+window.geometry("1280x720")
+window.configure(bg = "#FFFFFF")
+
+canvas = Canvas(window,bg = "#FFFFFF",height = 720,width = 1280,bd = 0,highlightthickness = 0,relief = "ridge")
+
+canvas.place(x = 0, y = 0)
+canvas.create_rectangle(0.0,0.0,384.0,720.0,fill="#36A692",outline="")
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(image=button_image_1,borderwidth=0,highlightthickness=0,command=get_photo,relief="flat"
+)
+button_1.place(x=33.0,y=136.0,width=316.0-4,height=124.0
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(image=button_image_2,borderwidth=0,highlightthickness=0,command=window.destroy,relief="flat")
+button_2.place(x=92.0,y=547.0,width=203.0,height=81.28125
+)
+image_label = Label(window)
+image_label.place(x=1280*0.3,y=0)
+get_photo()
+
+window.mainloop()
